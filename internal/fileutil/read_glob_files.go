c8bbda6ee9f6f993873776a6b86cfc748f61aa7f
Luis Eduardo Jeréz Girón <eduardo.devop@gmail.com>
2024-04-12 12:47:59 -0600

    Refactor fileutil package to use ReadGlobFiles instead of ReadFiles

---
+++ internal/fileutil/read_glob_files.go
@@ -0,0 +1,36 @@
+package fileutil
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+)
+
+func ReadGlobFiles(patterns []string) ([][]byte, error) {
+	paths := []string{}
+	for _, pattern := range patterns {
+		matches, err := filepath.Glob(pattern)
+		if err != nil {
+			return nil, fmt.Errorf("error globbing pattern %s: %w", pattern, err)
+		}
+		for _, match := range matches {
+			if !slices.Contains(paths, match) {
+				paths = append(paths, match)
+			}
+		}
+	}
+
+	fmt.Println(paths)
+
+	var dataSlice [][]byte
+	for _, path := range paths {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return nil, fmt.Errorf("error reading file %s: %w", path, err)
+		}
+		dataSlice = append(dataSlice, data)
+	}
+
+	return dataSlice, nil
+}
